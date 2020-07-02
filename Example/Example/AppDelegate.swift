b596a2f2e57a51017d85f3cfa92894a8ed2caadd
Inna Kuts <innalabynskaya@gmail.com>
2020-07-02 18:00:44 +0300

    example project

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Kuts on 02.07.2020.
+//  Copyright © 2020 PandaSDK. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Kuts on 02.07.2020.
+//  Copyright © 2020 PandaSDK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
