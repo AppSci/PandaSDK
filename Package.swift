d598e43a54b5cd91f7b1b8e8b3697df2322f0bf6
Inna Kuts <innalabynskaya@gmail.com>
2020-07-10 17:33:13 +0300

    Basic Package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PandaSDK",
+    platforms: [.iOS("11.2")],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PandaSDK",
+            targets: ["PandaSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", "4.8.0"..<"4.9.0")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "PandaSDK",
+            dependencies: ["NVActivityIndicatorView"]),
+    ]
+)
