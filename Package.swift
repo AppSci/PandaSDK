1436294d1677b8c2d9b970bbb2d201ec37adcadf
Inna Kuts <innalabynskaya@gmail.com>
2020-07-10 18:05:24 +0300

    ios requirements

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PandaSDK",
-    platforms: [.iOS("11.2")],
+    platforms: [.iOS("11.0")],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
