fdccb5fcfe2cd0c1ea98441320d99f4d85902d57
Inna Kuts <innalabynskaya@gmail.com>
2021-02-04 12:11:36 +0200

    Add Device Settings

---
+++ Sources/PandaSDK/DeviceSettings.swift
@@ -0,0 +1,20 @@
+//
+//  WeakObject.swift
+//  PandaSDK
+//
+//  Created by Kuts on 04.07.2020.
+//
+
+import Foundation
+
+struct DeviceSettings: Codable {
+    var pushToken: String
+    var advertisementIdentifier: String
+    var appsFlyerId: String
+    var customUserId: String
+    
+    static let `default` = DeviceSettings(pushToken: "",
+                                          advertisementIdentifier: "",
+                                          appsFlyerId: "",
+                                          customUserId: "")
+}
