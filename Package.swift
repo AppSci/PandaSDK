5e99a8cc199a350b91d9596880cb55661a227716
Oleksii Filobok <oleksii.filobok@gen.tech>
2022-02-05 17:41:41 +0200

    podspec update and packages update

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "PandaSDK",
-    platforms: [.iOS("11.2")],
+    platforms: [.iOS("13.0")],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "PandaSDK",
-            targets: ["PandaSDK"]),
+            targets: ["PandaSDK"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", "4.8.0"..<"4.9.0")
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", from: "5.1.1")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -22,6 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PandaSDK",
-            dependencies: ["NVActivityIndicatorView"]),
+            dependencies: ["NVActivityIndicatorView"]
+        )
     ]
 )
