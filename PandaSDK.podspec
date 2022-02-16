20a4758d4050af9a110cd056230c8929de5f92f5
Oleksii Filobok <oleksii.filobok@gen.tech>
2022-02-16 21:24:19 +0200

    Improvement for PandaPayload

--- PandaSDK.podspec
+++ PandaSDK.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "PandaSDK"
-  spec.version      = "0.5.2"
+  spec.version      = "0.6.1"
   spec.summary      = "Panda pod which will help us boost our revenue."
 
   # This description is used to generate tags and improve search results.
--- Sources/PandaSDK/Models/PandaPayload.swift
+++ Sources/PandaSDK/Models/PandaPayload.swift
@@ -8,26 +8,64 @@
 import UIKit
 
 public struct PandaPayload {
-    let shouldShowDefaultScreen: Bool
-    let screenBackgroundColor: UIColor?
-    let extraEventValues: [String: String]
-    let pageLoadingTimeout: TimeInterval
-    let htmlDownloadTimeout: TimeInterval?
-    let data: [String: Any]?
+    internal let shouldShowDefaultScreen: Bool
+    internal let screenBackgroundColor: UIColor?
+    internal let entryPoint: String?
+    internal let pageLoadingTimeout: TimeInterval
+    internal let htmlDownloadTimeout: TimeInterval?
+    internal let data: [String: Any]?
 
     public init(
         shouldShowDefaultScreen: Bool = true,
         screenBackgroundColor: UIColor? = nil,
-        extraEventValues: [String: String] = [:],
+        entryPoint: String? = nil,
         pageLoadingTimeout: TimeInterval = 3.0,
         htmlDownloadTimeout: TimeInterval? = nil,
-        data: [String: Any]? = nil
+        targetLanguage: String? = nil,
+        fromLanguage: String? = nil,
+        strings: [[String: String]]? = nil,
+        lessonTitle: String? = nil
     ) {
         self.shouldShowDefaultScreen = shouldShowDefaultScreen
         self.screenBackgroundColor = screenBackgroundColor
-        self.extraEventValues = extraEventValues
+        self.entryPoint = entryPoint
         self.pageLoadingTimeout = pageLoadingTimeout
         self.htmlDownloadTimeout = htmlDownloadTimeout
+        var data: [String: Any] = [:]
+        if let targetLanguage = targetLanguage {
+            data["target_language"] = targetLanguage
+        }
+        if let fromLanguage = fromLanguage {
+            data["from_language"] = fromLanguage
+        }
+
+        if let strings = strings {
+            data["strings"] = strings
+        }
+        if let lessonTitle = lessonTitle {
+            data["lesson_title"] = lessonTitle
+        }
+
         self.data = data
     }
 }
+
+//let strings = [
+//             ["feedback.first.title": L.Feedback.Title.text],
+//             ["feedback.first.placeholder": L.Feedback.Hint.text],
+//             ["feedback.first.button": L.Feedback.Button.text],
+//             ["feedback.second.title": L.SmartFeedback.Feedback.title],
+//             ["feedback.second.description": L.Feedback.Subtitle.text]
+//         ]
+//
+//         return [
+//             "background": UIColor.clear,
+//             "no_default": true,
+//             "data": [
+//         return PandaPayload(
+//             shouldShowDefaultScreen: false,
+//             screenBackgroundColor: .clear,
+//             data: [
+//                 "strings": strings,
+//                 "lesson_key": "",
+//                 "lesson_title": lesson.title
