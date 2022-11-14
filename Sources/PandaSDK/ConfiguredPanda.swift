126b7a13f7de530dad9550ea54a37010ca836638
Nick <nick.softwaredev@gmail.com>
2022-11-14 13:43:18 -0500

    Spacing

--- Sources/PandaSDK/ConfiguredPanda.swift
+++ Sources/PandaSDK/ConfiguredPanda.swift
@@ -512,7 +512,7 @@ final public class Panda: PandaProtocol, ObserverSupport {
         viewModel.onTerms = openTerms
         viewModel.onPolicy = openPolicy
         viewModel.onSubscriptionTerms = openSubscriptionTerms
-        viewModel.onPricesLoaded = {  [weak self] productIds, view in
+        viewModel.onPricesLoaded = { [weak self] productIds, view in
             self?.appStoreClient.fetchProducts(productIds: Set(productIds)) { result in
                 switch result {
                 case let .success(products):
